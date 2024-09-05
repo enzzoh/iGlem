de020fcac7a4f61ae1435957d61cf433476e2633
Enzzo Samuel <enzzosamuel742@gmail.com>
2024-09-05 14:37:09 -0300

    05/09

---
+++ test v01/test v01/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test_v01.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+    }
+}
